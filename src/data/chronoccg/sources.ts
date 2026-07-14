f9c2bd89a95e9fd7597cbbf390d8ec90d7324ea7
kiraplane <zhh0504@gmail.com>
2026-07-14 16:06:54 +0800

    chore: refresh growth iteration data

--- src/data/chronoccg/sources.ts
+++ src/data/chronoccg/sources.ts
@@ -1,6 +1,6 @@
 import type { DataSource } from './types';
 
-export const CHECKED_AT = '2026-07-11';
+export const CHECKED_AT = '2026-07-14';
 
 export const officialGameFacts = {
   name: 'Chrono CCG',
